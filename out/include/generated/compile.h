be583215136c52bee1736cf2f305b49e3deacde3
SnowWolf725 <snowwolf725@gmail.com>
2024-01-26 21:24:20 +0800

    add workflows

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#0 SMP PREEMPT Fri Jan 26 21:04:39 CST 2024"
-#define LINUX_COMPILE_BY "snowwolf725"
-#define LINUX_COMPILE_HOST "DESKTOP-6GOB0EK"
-#define LINUX_COMPILER "gcc version 11.4.0 (Ubuntu 11.4.0-1ubuntu1~22.04)"
